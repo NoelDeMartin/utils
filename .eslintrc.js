6f96636166efc2e61f0e22fff053a9314f4b8eb2
Noel De Martin <noeldemartin@gmail.com>
2020-12-08 18:31:39 +0100

    Extract eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,57 +4,9 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: [
-        '@typescript-eslint',
-    ],
+    extends: ['@noeldemartin/eslint-config-typescript'],
     ignorePatterns: [
         'dist',
         'scripts',
     ],
-    rules: {
-        '@typescript-eslint/no-explicit-any': [
-            'warn',
-            {
-                ignoreRestArgs: true,
-            },
-        ],
-        '@typescript-eslint/no-inferrable-types': 'off',
-        'comma-dangle': [
-            'error',
-            'always-multiline',
-        ],
-        'indent': [
-            'error',
-            4,
-        ],
-        'linebreak-style': [
-            'error',
-            'unix',
-        ],
-        'max-len': [
-            'error',
-            120,
-        ],
-        'quotes': [
-            'error',
-            'single',
-        ],
-        'quote-props': [
-            'error',
-            'consistent-as-needed',
-        ],
-        'semi': [
-            'error',
-            'always',
-        ],
-    },
 };
