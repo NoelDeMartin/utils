2ec8e71341786e1ee19f80f0380492c5bc62be9d
Noel De Martin <noeldemartin@gmail.com>
2021-01-14 21:01:51 +0100

    Accept promises in 'silenced' helper

--- src/helpers/promise_helpers.ts
+++ src/helpers/promise_helpers.ts
@@ -5,15 +5,22 @@ export function silenced<Args extends ClosureArgs, Result, Default>(
     operation: Closure<Args, Promise<Result>>,
     defaultValue: Default,
 ): Closure<Args, Promise<Result | Default>>;
+export function silenced<Result>(promise: Promise<Result>): Promise<Result | null>;
 export function silenced<Args extends ClosureArgs, Result, Default>(
     operation: Closure<Args, Promise<Result>>,
+    defaultValue: Default,
+): Closure<Args, Promise<Result | Default>>;
+export function silenced<Args extends ClosureArgs, Result, Default>(
+    operationOrPromise: Closure<Args, Promise<Result>> | Promise<Result>,
     defaultValue: Default | null = null,
-): Closure<Args, Promise<Result | Default | null>> {
-    return async (...args: Args) => {
-        try {
-            return await operation(...args);
-        } catch (error) {
-            return defaultValue || null;
-        }
-    };
+): Closure<Args, Promise<Result | Default | null>> | Promise<Result | Default | null> {
+    return typeof operationOrPromise !== 'function'
+        ? operationOrPromise.catch(() => defaultValue)
+        : async (...args: Args) => {
+            try {
+                return await operationOrPromise(...args);
+            } catch (error) {
+                return defaultValue || null;
+            }
+        };
 }
