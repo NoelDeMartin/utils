27d75b3e784e78180faba9e360bcbd065904893f
Noel De Martin <noeldemartin@gmail.com>
2021-09-29 20:03:21 +0200

    Allow indicating fail return type

--- src/helpers/error_helpers.ts
+++ src/helpers/error_helpers.ts
@@ -11,8 +11,8 @@ export function catchError(callback: () => unknown): Error | null {
 }
 
 /* eslint-disable @typescript-eslint/no-explicit-any, max-len */
-export function fail(message?: string): any;
-export function fail<T extends Error>(errorClass?: Constructor<T>, ...params: ConstructorParameters<Constructor<T>>): any;
+export function fail<T = any>(message?: string): T;
+export function fail<T = any, E extends Error = Error>(errorClass?: Constructor<E>, ...params: ConstructorParameters<Constructor<E>>): T;
 export function fail(messageOrClass: string | Constructor<Error> = 'Something went wrong', ...params: any[]): any {
     const errorClass = typeof messageOrClass === 'string' ? Error : messageOrClass;
 
