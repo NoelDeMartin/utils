42a9ecc935f8de0b7ef7c1d6af6677e57823eba9
Noel De Martin <noel@noeldemartin.com>
2022-01-15 09:51:40 +0100

    Implement file helpers

---
+++ src/helpers/file_helpers.ts
@@ -0,0 +1,17 @@
+export const enum FileMediaType {
+    JSON = 'application/json',
+    CSV = 'text/csv',
+}
+
+export function downloadFile(filename: string, content: string, mediaType: string): void {
+    const url = window.URL.createObjectURL(new Blob([content], { type: mediaType }));
+    const anchor = document.createElement('a');
+
+    anchor.style.display = 'none';
+    anchor.href = url;
+    anchor.download = filename;
+    document.body.appendChild(anchor);
+    anchor.click();
+    window.URL.revokeObjectURL(url);
+    document.body.removeChild(anchor);
+}
