12d8fdd10626ff88de9284ed6525352f4612d910
Noel De Martin <noel@noeldemartin.com>
2022-02-26 10:27:51 +0100

    Improve `compare` types

--- src/helpers/logical_helpers.ts
+++ src/helpers/logical_helpers.ts
@@ -1,4 +1,3 @@
-export function compare(a: unknown, b: unknown): 1 | -1 | 0 {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return (a as any) > (b as any) ? 1 : ((b as any) > (a as any) ? -1 : 0);
+export function compare<T>(a: T, b: T): 1 | -1 | 0 {
+    return a > b ? 1 : (b > a ? -1 : 0);
 }
