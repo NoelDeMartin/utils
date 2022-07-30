358018707f76deaa621891d9ccb26373e6019fed
Noel De Martin <noel@noeldemartin.com>
2022-07-30 12:33:39 +0200

    Implement closure type getters

--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -1,10 +1,12 @@
 export type ClassInstance<Class> = Class extends { new(...args: any[]): infer T } ? T : never;
 export type Closure<Args extends ClosureArgs = ClosureArgs, Result = unknown> = (...args: Args) => Result;
+export type ClosureResult = any; // eslint-disable-line @typescript-eslint/no-explicit-any
 export type ClosureArgs = any[]; // eslint-disable-line @typescript-eslint/no-explicit-any
 export type Falsy = null | undefined | false | 0;
 export type GetOptionalKeys<T> = { [K in keyof T]-?: Record<string, never> extends Pick<T, K> ? K : never }[keyof T];
 export type GetRequiredKeys<T> = { [K in keyof T]-?: T extends Record<K, T[K]> ? K : never }[keyof T];
-export type ReturnType<T extends (...args: any[]) => unknown> = T extends (...args: any[]) => infer R ? R : never;
+export type GetClosureArgs<T extends Closure> = T extends (...args: infer Args) => ClosureResult ? Args : never;
+export type GetClosureResult<T extends Closure> = T extends (...args: any[]) => infer Result ? Result : never;
 
 // Workaround for https://github.com/typescript-eslint/typescript-eslint/issues/3573
 export type Use<T> = {}; // eslint-disable-line @typescript-eslint/no-unused-vars
