01563633fd0946eb5d8f8589546addd11f7c171a
Noel De Martin <noel@noeldemartin.com>
2023-11-09 21:39:04 +0100

    Implement asyncMemo

--- src/helpers/caching_helpers.test.ts
+++ src/helpers/caching_helpers.test.ts
@@ -1,8 +1,51 @@
-import { once } from './caching_helpers';
+import { asyncMemo, once, setAsyncMemo } from './caching_helpers';
 
 describe('Cache helpers', () => {
 
-    it('caches results', () => {
+    it('caches async results', async () => {
+        // Arrange
+        let calls = 0;
+        const key = 'store';
+        const result = 42;
+        const operation = () => (calls++, Promise.resolve(result));
+
+        // Act
+        const results = await Promise.all([
+            asyncMemo(key, operation),
+            asyncMemo(key, operation),
+            asyncMemo(key, operation),
+        ]);
+
+        // Assert
+        expect(calls).toBe(1);
+
+        results.forEach(actualResult => expect(actualResult).toEqual(result));
+    });
+
+    it('sets cached async results', async () => {
+        // Arrange
+        let calls = 0;
+        const key = 'store';
+        const lazyResult = 42;
+        const eagerResult = 23;
+        const operation = () => (calls++, Promise.resolve(lazyResult));
+
+        // Act
+        setAsyncMemo(key, eagerResult);
+
+        const results = await Promise.all([
+            asyncMemo(key, operation),
+            asyncMemo(key, operation),
+            asyncMemo(key, operation),
+        ]);
+
+        // Assert
+        expect(calls).toBe(0);
+
+        results.forEach(actualResult => expect(actualResult).toEqual(eagerResult));
+    });
+
+    it('caches results using once', () => {
         // Arrange.
         let count = 0;
         const counter = () => count++;
--- src/helpers/caching_helpers.ts
+++ src/helpers/caching_helpers.ts
@@ -1,8 +1,23 @@
+import PromisedValue from '@/helpers/PromisedValue';
 import type { Closure, ClosureArgs } from '@/types/index';
+
+const asyncCache: Record<string, PromisedValue> = {};
+
+export function asyncMemo<T>(key: string, operation: () => Promise<T>): Promise<T> {
+    asyncCache[key] = asyncCache[key] ?? PromisedValue.from<unknown>(operation());
+
+    return asyncCache[key] as Promise<T>;
+}
 
 export function once<P extends ClosureArgs, R>(operation: Closure<P, R>): Closure<P, R> {
     const memo: { result?: R } = {};
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     return (...params) => 'result' in memo ? memo.result : (memo.result = operation(...params)) as any;
+}
+
+export function setAsyncMemo(key: string, value: unknown): void {
+    asyncCache[key] = asyncCache[key] ?? new PromisedValue();
+
+    asyncCache[key]?.resolve(value);
 }
