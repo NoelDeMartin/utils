2b265eb68e56e14f0ba17c100c2068aa5654532f
Noel De Martin <noel@noeldemartin.com>
2024-07-06 10:45:30 +0200

    Extract required proxy handler class

--- src/fluent/required.ts
+++ src/fluent/required.ts
@@ -1,6 +1,32 @@
 import { fail } from '@/helpers/error_helpers';
 import type { ValueWithoutEmpty } from '@/helpers/object_helpers';
 
+class RequiredHandler<Target extends object> implements ProxyHandler<Target> {
+
+    constructor(private errorMessage: string, private getValue: () => Target) {}
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public get(_: Target, property: string | symbol, receiver: any): any {
+        const obj = this.getValue() ?? fail<ValueWithoutEmpty<Target>>(this.errorMessage);
+
+        return Reflect.get(obj, property, receiver);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    public set?(_: Target, property: string | symbol, value: any, receiver: any): boolean {
+        const obj = this.getValue() ?? fail<ValueWithoutEmpty<Target>>(this.errorMessage);
+
+        return Reflect.set(obj, property, value, receiver);
+    }
+
+    public getPrototypeOf(): object | null {
+        const obj = this.getValue() ?? fail<ValueWithoutEmpty<Target>>(this.errorMessage);
+
+        return Object.getPrototypeOf(obj);
+    }
+
+}
+
 export function required<T extends object | null | undefined>(
     getValue: () => T,
     errorMessage?: string
@@ -10,27 +36,9 @@ export function required<T>(value: (() => T) | T, errorMessage?: string): ValueW
     errorMessage ??= 'Required value is missing';
 
     if (typeof value === 'function') {
-        const getValue = value as unknown as () => object;
-
         return new Proxy(
             {},
-            {
-                get(_, property, receiver) {
-                    const obj = getValue() ?? fail<ValueWithoutEmpty<T>>(errorMessage);
-
-                    return Reflect.get(obj, property, receiver);
-                },
-                set(_, property, value, receiver) {
-                    const obj = getValue() ?? fail<ValueWithoutEmpty<T>>(errorMessage);
-
-                    return Reflect.set(obj, property, value, receiver);
-                },
-                getPrototypeOf() {
-                    const obj = getValue() ?? fail<ValueWithoutEmpty<T>>(errorMessage);
-
-                    return Object.getPrototypeOf(obj);
-                },
-            },
+            new RequiredHandler(errorMessage, value as unknown as () => object),
         ) as ValueWithoutEmpty<T>;
     }
 
