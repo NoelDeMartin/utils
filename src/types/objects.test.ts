d65537f8791025d5f16ef342cd78e841f6a3c20c
Noel De Martin <noel@noeldemartin.com>
2025-04-21 09:56:56 +0200

    Improve DeepKey types

--- src/types/objects.test.ts
+++ src/types/objects.test.ts
@@ -14,6 +14,17 @@ interface Post {
         name: string;
     };
     related: Post[];
+    categories?: { id: string }[];
+    deep: {
+        one: {
+            two: {
+                three: {
+                    four: string;
+                };
+            };
+        };
+    };
+    publish(): void;
 }
 
 describe('Object types', () => {
@@ -21,10 +32,25 @@ describe('Object types', () => {
     it(
         'Has correct types',
         tt<
-            | Expect<Equals<DeepKeyOf<Post>, 'id' | 'title' | 'author' | 'author.id' | 'author.name' | 'related'>>
+            | Expect<
+                  Equals<
+                      DeepKeyOf<Post>,
+                      | 'id'
+                      | 'title'
+                      | 'author'
+                      | 'author.id'
+                      | 'author.name'
+                      | 'related'
+                      | 'categories'
+                      | 'deep'
+                      | 'deep.one'
+                      | 'deep.one.two'
+                  >
+              >
             | Expect<Equals<DeepValue<Post, 'author'>, { id: number; name: string }>>
             | Expect<Equals<DeepValue<Post, 'author.name'>, string>>
             | Expect<Equals<DeepValue<Post, 'related'>, Post[]>>
+            | Expect<Equals<DeepValue<Post, 'categories'>, { id: string }[] | undefined>>
             | true
         >(),
     );
--- src/types/objects.ts
+++ src/types/objects.ts
@@ -1,10 +1,21 @@
 import type { ArrayDeepItem, ArrayIncludes } from '@noeldemartin/utils/types/arrays';
+import type { Closure } from '@noeldemartin/utils/types/helpers';
 
-export type DeepKeyOf<T, P extends unknown[] = []> = T extends object
-    ? ArrayIncludes<P, ArrayDeepItem<T>> extends true
-        ? never
-        : { [K in keyof T]: `${Exclude<K, symbol>}${'' | `.${DeepKeyOf<T[K], [...P, T]>}`}` }[keyof T]
-    : never;
+export type DeepKeyOf<T, P extends unknown[] = [], Depth extends number = 2> = NonNullable<
+    T extends object
+        ? ArrayIncludes<P, ArrayDeepItem<T>> extends true
+            ? never
+            : {
+                  [K in keyof T]: T[K] extends Closure
+                      ? never
+                      : NonNullable<T[K]> extends Array<unknown>
+                        ? Exclude<K, symbol>
+                        : Depth extends -1
+                          ? never
+                          : `${Exclude<K, symbol>}${'' | `.${DeepKeyOf<T[K], [...P, T], [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9][Depth]>}`}`;
+              }[keyof T]
+        : never
+>;
 
 export type DeepValue<T extends object, K> = K extends `${infer Key}.${infer Rest}`
     ? Key extends keyof T
