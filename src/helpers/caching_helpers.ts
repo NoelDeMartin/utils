49b86b7e046851827ad056515625af23310b76a5
Noel De Martin <noel@noeldemartin.com>
2025-05-24 10:06:31 +0200

    Implement weak memo

--- src/helpers/caching_helpers.ts
+++ src/helpers/caching_helpers.ts
@@ -1,9 +1,11 @@
 import PromisedValue from '@noeldemartin/utils/helpers/PromisedValue';
 import { arrayFrom } from '@noeldemartin/utils/helpers/array_helpers';
+import { fail } from '@noeldemartin/utils/helpers/error_helpers';
 import type { Closure, ClosureArgs } from '@noeldemartin/utils/types';
 
 const cache: Record<string, unknown> = {};
 const asyncCache: Record<string, PromisedValue> = {};
+const weakCache: Record<string, WeakMap<object, unknown>> = {};
 
 export function asyncMemo<T>(key: string, operation: () => Promise<T>): Promise<T> {
     asyncCache[key] = asyncCache[key] ?? PromisedValue.from<unknown>(operation());
@@ -24,6 +26,26 @@ export function once<P extends ClosureArgs, R>(operation: Closure<P, R>): Closur
     return (...params) => ('result' in memory ? memory.result : ((memory.result = operation(...params)) as any));
 }
 
+export function weakMemo<T>(name: string, key: object, operation: () => T): T {
+    if (!weakCache[name]?.has(key)) {
+        setWeakMemo(name, key, operation());
+    }
+
+    return (weakCache[name]?.get(key) as T) ?? fail();
+}
+
+export function getAsyncMemo<T>(key: string): Promise<T | null> {
+    return Promise.resolve((asyncCache[key] as T) ?? null);
+}
+
+export function getMemo<T>(key: string): T | null {
+    return (cache[key] as T) ?? null;
+}
+
+export function getWeakMemo<T>(name: string, key: object): T | null {
+    return (weakCache[name]?.get(key) as T) ?? null;
+}
+
 export function resetAsyncMemo(key?: string): void {
     const keys = arrayFrom(key ?? Object.keys(asyncCache));
 
@@ -36,6 +58,10 @@ export function resetMemo(key?: string): void {
     keys.forEach((k) => delete cache[k]);
 }
 
+export function resetWeakMemo(name: string, key: object): void {
+    weakCache[name]?.delete(key);
+}
+
 export function setAsyncMemo(key: string, value: unknown): void {
     asyncCache[key] = asyncCache[key] ?? new PromisedValue();
 
@@ -44,4 +70,10 @@ export function setAsyncMemo(key: string, value: unknown): void {
 
 export function setMemo(key: string, value: unknown): void {
     cache[key] = value;
+}
+
+export function setWeakMemo(name: string, key: object, value: unknown): void {
+    const memo = (weakCache[name] ??= new WeakMap());
+
+    memo.set(key, value);
 }
